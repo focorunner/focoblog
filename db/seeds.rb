9394c0fc0a8391973dfc88936e9c398cf90d878f
Mark Coleman <mark@focorunner.com>
2015-03-10 16:42:44 -0600

    update seed file for heroku deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-User.create!(name:  "First User",
-             email: "example@example.com",
+User.create!(name:  "Admin",
+             email: "admin@example.com",
              password:              "foobar",
              password_confirmation: "foobar",
              admin:     true,
              activated: true,
              activated_at: Time.zone.now)
-
+=begin
 User.create!(name:  "Second User",
              email: "second@example.com",
              password:              "foobar",
@@ -32,3 +32,4 @@ Article.create!(title: "Autem quasi molestiae commodi ipsa?",
 Article.create!(title: "Voluptatibus quos quas voluptatum veritatis!",
                 body: "Ex officia, excepturi aspernatur dolorem eos? Eligendi voluptatum adipisci alias vel error modi, ipsum repellendus. Eveniet laboriosam officia necessitatibus temporibus est facere dolorem, iure minus labore. Enim repellendus dolorem dicta dolore neque numquam nobis optio sunt, quisquam commodi. Assumenda sunt dolorem minima, accusantium dolor id corporis perferendis animi tempora, soluta molestiae, beatae tempore fuga omnis sapiente neque voluptatibus quos recusandae possimus. Fugit amet earum eveniet expedita sequi, excepturi totam recusandae, maxime minus voluptatum iste quibusdam dolor consequatur sed molestias perspiciatis, perferendis impedit nulla! Ratione ad, in sed natus voluptas labore rem id voluptate tempora aliquid eum minima qui recusandae excepturi esse, inventore saepe doloribus suscipit incidunt fugiat facere, totam ex! Velit deserunt inventore vel tempore praesentium, ipsa, dolores doloremque rerum commodi aperiam, aliquid. Vitae doloribus eligendi impedit, ducimus temporibus quaerat, consequatur alias cupiditate rerum repudiandae reiciendis. Facere quam error dolores molestiae voluptatum nesciunt! Possimus ut similique quidem dolore a facilis cum veniam quisquam repellendus, quis, omnis cupiditate, minima! Tempore dolores non natus vitae mollitia, amet voluptas quae minima numquam esse neque magnam accusantium, quis, odio voluptates omnis illo sint deleniti enim inventore. Eum quo repudiandae voluptas tempora natus, dolore saepe assumenda quaerat. Error dignissimos magnam totam id quod nobis necessitatibus cupiditate reprehenderit minus, aliquam. Ipsam laborum, rerum similique debitis illum excepturi facilis quis nisi dicta non obcaecati beatae, tempora laudantium maxime optio vitae blanditiis atque consectetur doloremque ad pariatur facere aut nulla, et consequuntur. Omnis ullam adipisci pariatur perspiciatis laudantium mollitia praesentium consequuntur nesciunt iure debitis delectus eveniet accusamus sed aliquid quasi eum in dignissimos sint exercitationem, inventore, eos temporibus commodi expedita id. Molestiae impedit repellendus nobis dolore, molestias temporibus ea, error labore, tenetur accusantium laborum facere. Ipsa et eaque nihil incidunt beatae minus voluptatum modi, repudiandae quia molestiae sed necessitatibus animi nulla, eligendi, ab at quasi consequatur quisquam voluptatibus autem fuga expedita inventore doloremque molestias? Modi dolorem voluptatem magni, aut aspernatur blanditiis explicabo fuga. Eos explicabo eius similique voluptatem sunt magni necessitatibus repudiandae, fuga consequuntur consequatur est minus quos voluptate earum cupiditate laboriosam nisi, soluta tenetur excepturi, praesentium. Quod soluta quasi asperiores omnis cupiditate ipsa voluptas culpa aspernatur perferendis, molestiae magni voluptatibus, adipisci doloribus enim laboriosam aut vitae voluptates rerum iusto modi harum ullam magnam odit explicabo obcaecati.",
                 user_id: 1)
+=end
