0362fe9e51107650190eb3073a80af8ed2a71a66
Mark Coleman <mark@focorunner.com>
2015-03-10 20:15:37 -0600

    More Mandrill configuration for Heroku :

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,18 +65,17 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
   host = 'agile-garden-2573.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :port           => '587',
-    :address        => 'smtp.mandrillapp.net',
-    :user_name      => ENV['MANDRILL_USERNAME'],
-    :password       => ENV['MANDRILL_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true,
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
     :authentication => :plain
   }
+  ActionMailer::Base.delivery_method = :smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
