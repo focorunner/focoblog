db14331974ee4b4639bf65dce845dd4613f56933
Mark Coleman <mark@focorunner.com>
2015-03-16 15:04:24 -0600

    Add author boolean to users, plus some css improvements.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150307215819) do
+ActiveRecord::Schema.define(version: 20150316164601) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20150307215819) do
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
     t.string   "bio"
+    t.boolean  "author"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
