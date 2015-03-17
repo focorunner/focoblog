92a6fcd8b77ff98af8704a5383dcd44e98d79af0
Mark Coleman <mark@focorunner.com>
2015-03-16 19:35:15 -0600

    Add tab bar graph, add tabs to article index, refactor comment count to helper, reformat article meta information.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,9 @@
 module ArticlesHelper
+  def comment_count(comments)
+    if comments.present? 
+      content_tag("strong", pluralize(comments.count, "Comment"))
+    else
+      "No Comments"
+    end
+  end
 end
