1d204abc0b48918387cd01899a9cf5431002c124
Mark Coleman <mark@focorunner.com>
2015-06-18 21:43:19 -0600

    Fix for password reset form error. Fix for error on attempted delete of articles with related comments and or tags.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,10 @@
 class Article < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
-  has_many :taggings
+  has_many :comments, dependent: :destroy
+  has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
   validates :title, presence: true
   validates :body, presence: true
-
 
   def tag_list
     tags.join(", ")
